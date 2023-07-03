325ed2d12bbc34ccb564ed22cd1be76243e4d1d5
JJong <63280700+entere77@users.noreply.github.com>
2023-07-03 15:00:20 +0900

    Create 6044  [기초-산술연산] 정수 2개 입력받아 자동 계산하기.py

---
+++ 6000/6044  [기초-산술연산] 정수 2개 입력받아 자동 계산하기.py
@@ -0,0 +1,7 @@
+﻿input1, input2 = map(int, input().split())
+print(input1 + input2)
+print(input1 - input2)
+print(input1 * input2)
+print(input1 // input2)
+print(input1 % input2)
+print(format((input1 / input2), ".2f"))
