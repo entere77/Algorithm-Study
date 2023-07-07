4acf5dcd44fa7554538f0d28426539c325907c9d
JJong <63280700+entere77@users.noreply.github.com>
2023-07-07 12:32:18 +0900

    Create 6083  [기초-종합] 빛 섞어 색 만들기.py

---
+++ 6000/6083  [기초-종합] 빛 섞어 색 만들기.py
@@ -0,0 +1,8 @@
+﻿input_list = list(map(int, input().split()))
+total = 0
+for i in range(input_list[0]):
+    for j in range(input_list[1]):
+        for k in range(input_list[2]):
+            print(f"{i} {j} {k}")
+            total += 1
+print(total)
